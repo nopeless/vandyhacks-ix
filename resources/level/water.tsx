43a79bffc89d54a8804a15b663ff505cac8f39b5
Hudson Q <hudsonx86@macbook.local>
2022-10-22 16:56:37 -0500

    namechange

--- resources/level/water.tsx
+++ resources/level/water.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="water" tilewidth="8" tileheight="8" spacing="1" margin="1" tilecount="64" columns="16">
- <image source="water.png" width="145" height="37"/>
+ <image source="watertileset.png" width="145" height="37"/>
 </tileset>
