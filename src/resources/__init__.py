331804d9555dcf3426c65f0fe415e46742786a0d
Hudson Q <hudsonx86@macbook.local>
2022-10-23 07:46:56 -0500

    fix fall and dash

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -14,9 +14,8 @@ level = TMXManager("resources/level")
 images.player.walk = SourceGif(images.player.sheet.walk, 5).instance
 images.player.run = SourceGif(images.player.sheet.run, 5).instance
 images.player.idle = SourceGif(images.player.sheet.idle, 10).instance
-# TODO fix this
-images.player.fall = SourceGif(images.player.sheet.idle, 10).instance
-images.player.dash = SourceGif(images.player.sheet.idle, 10).instance
+images.player.fall = SourceGif(images.player.sheet.fall, 10).instance
+images.player.dash = SourceGif(images.player.sheet.dash, 10).instance
 
 images.cleaner.idle = SourceGif(images.cleaner.sheet.idle, 10).instance
 images.cleaner.sucking = SourceGif(images.cleaner.sheet.sucking, 30).instance
