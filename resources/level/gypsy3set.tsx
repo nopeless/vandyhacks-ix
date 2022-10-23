e74a0deac0d5bf485a8e39503cef20c133065bde
Hudson Q <hudsonx86@macbook.local>
2022-10-22 22:35:13 -0500

    fix2

--- resources/level/gypsy3set.tsx
+++ resources/level/gypsy3set.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="gypsy3set" tilewidth="34" tileheight="32" tilecount="6" columns="6">
- <image source="../../../vandyhacks-ix/resources/level/gypsy.png" width="204" height="34"/>
+ <image source="./gypsy.png" width="204" height="34"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="200"/>
