dda4eb70a2a41977f4d858337d409ebe9b17cc80
nopeless <38830903+nopeless@users.noreply.github.com>
2022-10-23 04:20:36 -0500

    logging messages

--- src/game/world_load.py
+++ src/game/world_load.py
@@ -27,3 +27,4 @@ def load(world):
         for x in range(0, config.tmx.width, 4):
             for y in range(0, config.tmx.width, 4):
                 world.texts.add(fac(x * 8, y * 8, f"{x}x{y}"))
+        logging.info("added debug tile coordinates")
