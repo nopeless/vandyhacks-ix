93d473cf31fe8e061e112f7d90236b45119afabe
Hudson Q <hudsonx86@macbook.local>
2022-10-22 19:59:09 -0500

    Overhaul level

---
+++ resources/level/waterset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="waterset" tilewidth="8" tileheight="8" spacing="1" margin="1" tilecount="64" columns="16">
+ <image source="water.png" width="145" height="37"/>
+</tileset>
