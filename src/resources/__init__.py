d787cb26e55c77cf4b567fee8b9a0b8cb8f88767
nopeless <38830903+nopeless@users.noreply.github.com>
2022-10-22 23:07:23 -0500

    add bgm export

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -2,15 +2,19 @@ from types import SimpleNamespace
 
 from managers import *
 
-from lib import Player
+from lib import Player, Image, SourceGif
 
 images = ImageManager("resources/images")
 sfx = PygameSoundManager("resources/audio/sfx")
+bgm = PygameSoundManager("resources/audio/bgm")
 
 level = TMXManager("resources/level")
 
+images.player_walking_test = SourceGif(images.player.moves_b.moves, 15).instance
+
 # Load sprites here
 sprites = SimpleNamespace()
+
 
 sprites.player = Player(images.player.hitbox)
 sprites.player.hitbox = images.player.hitbox
